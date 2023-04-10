ff203a609e68a8186f94e72f9653cfffb0a5f365
poka111 <pokabu123@icloud.com>
2023-04-10 13:17:38 +0900

    コメントアウト

--- src/main/java/AnotherDimensionCore/client/network/ClientNetworks.java
+++ src/main/java/AnotherDimensionCore/client/network/ClientNetworks.java
@@ -10,19 +10,19 @@ public class ClientNetworks{
 
     }
 
-    public static void sendPressGKey(){
-        ClientPlayNetworking.send(AnotherDimensionCoreMod.id("press_g_key"),PacketByteBufs.empty());
-    }
+    // public static void sendPressGKey(){
+    //     ClientPlayNetworking.send(AnotherDimensionCoreMod.id("press_g_key"),PacketByteBufs.empty());
+    // }
 
-    public static void sendPressF_1Key(){
-        ClientPlayNetworking.send(AnotherDimensionCoreMod.id("press_f_1key"),PacketByteBufs.empty());
-    }
+    // public static void sendPressF_1Key(){
+    //     ClientPlayNetworking.send(AnotherDimensionCoreMod.id("press_f_1key"),PacketByteBufs.empty());
+    // }
 
-    public static void sendPressF_2Key(){
-        ClientPlayNetworking.send(AnotherDimensionCoreMod.id("press_f_2key"),PacketByteBufs.empty());
-    }
+    // public static void sendPressF_2Key(){
+    //     ClientPlayNetworking.send(AnotherDimensionCoreMod.id("press_f_2key"),PacketByteBufs.empty());
+    // }
 
-    public static void sendPressF_3Key(){
-        ClientPlayNetworking.send(AnotherDimensionCoreMod.id("press_f_3key"),PacketByteBufs.empty());
-    }
+    // public static void sendPressF_3Key(){
+    //     ClientPlayNetworking.send(AnotherDimensionCoreMod.id("press_f_3key"),PacketByteBufs.empty());
+    // }
 }
